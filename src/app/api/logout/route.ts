c9235cb7e7b025d6a42637e60c2462570d54c9a0
hasnain ali <khasnainali8@gmail.com>
2024-07-24 15:12:38 +0500

    Isssue Fixed

--- src/app/api/logout/route.ts
+++ src/app/api/logout/route.ts
@@ -1,13 +1,18 @@
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function POST(req: NextRequest) {
-  const res = NextResponse.redirect(new URL('/login', req.url));
-  res.cookies.set('token', '', {
-    maxAge: 0,
-    httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
-    sameSite: 'strict',  // Set to 'strict'
-  });
+  try {
+    const res = new NextResponse('User Logout', { status: 200 });
+    res.cookies.set('token', '', {
+      maxAge: 0,
+      httpOnly: false,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: 'strict',  // Set to 'strict'
+    });
 
-  return res;
+    return res;
+  } catch (error) {
+    console.error('Error in POST request:', error);
+    return new NextResponse('Internal Server Error', { status: 500 });
+  }
 }
